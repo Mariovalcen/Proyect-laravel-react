2b9aa86f2aab398444d9935ad6a04c1833216e36
Mariovalcen <mariovalcen@gmail.com>
2023-02-19 00:28:00 +0100

    Creating Resources/js/pages/blog/index. Working with tailwind styles

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class PostController extends Controller
 {
 
     public function index() 
     {
-      return response()->json([
-        "message" => "Bootcamp con laravel"
+      return Inertia::render('Blogs/Index',[
+
       ]);
     }
 
