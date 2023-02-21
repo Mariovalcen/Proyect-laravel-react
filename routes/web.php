7454ccc478fb760543529db65cac082920c3cd1e
Mariovalcen <mariovalcen@gmail.com>
2023-02-21 02:10:09 +0100

    Testing create components and their respective routes

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,10 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/prueba', function () {
+    return Inertia::render('Prueba');
+})->middleware(['auth', 'verified'])->name('prueba');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
